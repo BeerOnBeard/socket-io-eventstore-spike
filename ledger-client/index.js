c7b120d1a64991f5694ba27be2517f83a9437bee
BeerOnBeard <repoguy156+github@gmail.com>
2018-12-15 18:23:42 +0200

    Pulling requires to the top to follow general style of other NodeJS projects

--- ledger-client/index.js
+++ ledger-client/index.js
@@ -11,27 +11,22 @@ const eventStoreConfig = {
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const EventStore = require('event-store-client');
+const LedgerRepository = require('./LedgerRepository');
+const LedgerController = require('./LedgerController');
+
+const eventStoreConnection = new EventStore.Connection({ host: eventStoreConfig.host, port: eventStoreConfig.port });
+const ledgerRepository = new LedgerRepository(eventStoreConnection, eventStoreConfig.credentials.username, eventStoreConfig.credentials.password);
+const ledgerController = new LedgerController(ledgerRepository);
 
 app.use(express.static('public'));
 app.use(express.json());
-
-const EventStore = require('event-store-client');
-const eventStoreConnection = new EventStore.Connection({ host: eventStoreConfig.host, port: eventStoreConfig.port });
-
-const LedgerRepository = require('./LedgerRepository');
-const ledgerRepository = new LedgerRepository(
-  eventStoreConnection,
-  eventStoreConfig.credentials.username,
-  eventStoreConfig.credentials.password);
-
-const LedgerController = require('./LedgerController');
-const ledgerController = new LedgerController(ledgerRepository);
 app.post('/ledgers', (...arguments) => ledgerController.create(...arguments))
   .get('/ledgers/:ledgerId', (...arguments) => ledgerController.get(...arguments))
   .post('/ledgers/:ledgerId/increment', (...arguments) => ledgerController.increment(...arguments))
   .post('/ledgers/:ledgerId/decrement', (...arguments) => ledgerController.decrement(...arguments));
 
-const io = require('socket.io')(server);
 io.on('connection', socket => {
   socket.on('set-ledger', ledger => {
     socket.leaveAll();
